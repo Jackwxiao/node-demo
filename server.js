88efafa3880e8b63f00d796199fccfd8e463a88e
wuxiao <1291383707@qq.com>
2019-08-21 20:25:45 +0800

    修改server.js文件

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var http = require('http')
 var fs = require('fs')
 var url = require('url')
@@ -10,40 +11,30 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-	console.log('HTTP 路径为\n' + path)
-  	if(path == '/style.css'){
-	response.setHeader('Content-Type', 'text/css; charset=utf-8')
-	response.write('body{background-color: #ddd;}h1{color: red;}')
-	response.end()
-	}else if(path == '/main.js'){
-	response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-	response.write('alert("这是JS执行的")')
-	response.end()
-	}else if(path == '/'){
-	response.setHeader('Content-Type', 'text/html; charset=utf-8')
-	response.write('<!DOCTYPE>\n<html>'  + 
-	'<head><link rel="stylesheet" href="/style.css">' +
-	'</head><body>'  +
-	'<h1>你好</h1>' +
-	'<script src="/main.js"></script>' +
-	'</body></html>')
-	response.end()
-	}else{
-	response.statusCode = 404
-	response.end()
-	}
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
+    response.end()
+  }
 
-	/******** 代码结束，下面不要看 ************/
+  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
